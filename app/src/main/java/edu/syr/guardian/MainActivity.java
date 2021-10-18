01204381f12205ab2f8932c907e4f9035649e8c5
dkshkb <48193062+dkshkb@users.noreply.github.com>
2021-10-18 18:00:48 -0400

    Add the Map activity, but got a bug (map not showing well)

--- app/src/main/java/edu/syr/guardian/MainActivity.java
+++ app/src/main/java/edu/syr/guardian/MainActivity.java
@@ -5,15 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mMapButton;
+    Button mLogOutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLogOutButton = findViewById(R.id.main_button_logout);
+        mMapButton = findViewById(R.id.button_show_map);
+
+        mLogOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout(mLogOutButton);
+            }
+        });
+
+        mMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Map.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void logout(View view) {
@@ -22,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
 }
---
+++ app/src/main/java/edu/syr/guardian/Map.java
@@ -0,0 +1,39 @@
+package edu.syr.guardian;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Map extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng TechCtr = new LatLng(43.03760745511544, -76.13050018452081);
+        map.addMarker(new MarkerOptions().position(TechCtr).title("My Location"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(TechCtr));
+    }
+}
