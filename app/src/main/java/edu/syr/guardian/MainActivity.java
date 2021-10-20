de4848b32e4a4e03a613a1f57f55eacfdaec965a
dkshkb <48193062+dkshkb@users.noreply.github.com>
2021-10-20 17:05:53 -0400

    Google Map not showing bug fixed Two pinned locations on Google Map

--- app/src/main/java/edu/syr/guardian/MainActivity.java
+++ app/src/main/java/edu/syr/guardian/MainActivity.java
@@ -1,12 +1,19 @@
 package edu.syr.guardian;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
     Button mMapButton;
     Button mLogOutButton;
 
+    private static final String TAG =  "Mainactivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLogOutButton = findViewById(R.id.main_button_logout);
-        mMapButton = findViewById(R.id.button_show_map);
 
         mLogOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,13 +37,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mMapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Map.class);
-                startActivity(intent);
-            }
-        });
+        if (isServicesOK()){
+            Log.d(TAG, "Map Service is OK");
+            init();
+        }
+
+
 
     }
 
@@ -43,6 +51,44 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), Login.class);
         startActivity(intent);
         finish();
+    }
+
+    public void init(){
+        mMapButton = findViewById(R.id.button_show_map);
+        mMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Map.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK()
+    {
+        //checking google play service availibility
+        Log.d(TAG, "checking google version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        //check if the service connection is success
+        if (available == ConnectionResult.SUCCESS)
+        {
+            Log.d(TAG, "Google API services available");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available))
+        {
+            // checking if the error is solvable
+            Log.d(TAG, "error, we can fix it");
+            Dialog dialog =  GoogleApiAvailability.getInstance().getErrorDialog(this, available, ERROR_DIALOG_REQUEST);
+        }
+        else
+        {
+            //if cannot solve, just tell the user
+            Toast.makeText(this, "Google API service not vaialable. Cannot fix", Toast.LENGTH_LONG).show();
+        }
+
+        return false;
     }
 
 }
--- app/src/main/java/edu/syr/guardian/Map.java
+++ app/src/main/java/edu/syr/guardian/Map.java
@@ -1,11 +1,18 @@
 package edu.syr.guardian;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,24 +23,94 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Map extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
+//    GoogleMap map;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_map);
+//
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+//    }
+//
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//        map = googleMap;
+//
+//        LatLng TechCtr = new LatLng(43.03760745511544, -76.13050018452081);
+//        map.addMarker(new MarkerOptions().position(TechCtr).title("My Location"));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(TechCtr));
+//    }
+
+    // Callback interface for when the map is ready to be used.
+    //Once an instance of this interface is set on a MapFragment or MapView object,
+    // the onMapReady(GoogleMap) method is triggered when the map is ready to be used and
+    // provides a non-null instance of GoogleMap.
+
+    public static final int LOCATION_PERMISSION_REQUEST_CODE  = 1234;
+    private static String FINE_LOCATION =  Manifest.permission.ACCESS_FINE_LOCATION;
+    private boolean mLocationPermissionGranted =  false;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        //check for permission
+        getLocationPermission();
+        Log.d(TAG, "inside onCreate");
+        initMap();
+    }
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    private void initMap()
+    {
+        // put log"inside intiMap"
+        Log.d(TAG, "inside intiMap");
+        SupportMapFragment mapFragment =  (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(Map.this);
+    }
+
+    //this callback method is called when permission request comeas back
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(TAG, "inside onRequestPermissionsResult");
+        mLocationPermissionGranted = false;
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    mLocationPermissionGranted = true;
+                    //initialzie the map fragment
+                    initMap();
+                }
+
+        }
+    }
+
+    public void getLocationPermission()
+    {
+        String[] permissions =  {Manifest.permission.ACCESS_FINE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            mLocationPermissionGranted =  true;
+        else
+        {
+            //ask for permission
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        map = googleMap;
-
+        Log.d(TAG, "Map is ready!");
+        mMap =  googleMap;
         LatLng TechCtr = new LatLng(43.03760745511544, -76.13050018452081);
-        map.addMarker(new MarkerOptions().position(TechCtr).title("My Location"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(TechCtr));
+        LatLng HoL = new LatLng(43.03878212486239, -76.13454515767154);
+        mMap.addMarker(new MarkerOptions().position(TechCtr).title("My Location"));
+        mMap.addMarker(new MarkerOptions().position(HoL).title("User Location"));
+        Log.d(TAG, "Marked added");
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TechCtr));
     }
 }
